fa3a0583a60ae1a8154e0cce2d04cdded0c4af71
Mikaeel <95021467+Mikaeelmoha123@users.noreply.github.com>
2022-11-12 01:14:32 -0800

    Add files via upload
    Just using cout<<

---
+++ Simple details output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string FirstName, LastName, FullName;
+    cout << "Your First Name is Alex \n";
+    cout << "Your Last Name is Choi \n";
+    cout << "Your Full Name is Alex Choi \n";
+    cout << "Your Favorite meal is a Tomahawk stake \n";
+    cout << "Your Favorite movie is Top Gun Maverick";
+}
